6779eee6a72ae46b1214524ca2e01e0470935e81
james <jameson_andrade@hotmail.com.br>
2024-01-05 14:34:26 -0400

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# certbot-nginx
+<div align="center" id="top">
+  <img src="./.github/app.gif" alt="Api_2" />
+
+&#xa0;
+
+</div>
+
+<h1 align="center">INSTALADOR DO NGINX COM CERTBOT</h1>
+
+<p align="center">
+</p>
+
+<!-- Status -->
+
+<hr>
+
+<p align="center">
+  <a href="#dart-sobre">Sobre</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requisitos">Requisitos</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-instalação">Instalação</a> &#xa0; | &#xa0;
+  <a href="https://github.com/jamesandrade" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: Sobre
+
+O PRESENTE SCRIPT VISA AUTOMATIZAR A INSTALAÇÃO DO NGINX COM CERTIFICADO LET'S ENCRYPT GERADO PELO CERTBOT E INSTALAR UMA ROTINA PARA RENOVAR O CERTIFICADO DA APLICAÇÃO
+
+## :white_check_mark: Requisitos
+
+Para utilizar esse projeto, você precisará estar em um ambiente Linux, com [Git](https://git-scm.com), [Docker](https://www.docker.com/) e [Docker-Compose](https://docs.docker.com/compose/gettingstarted/) instalados.
+
+## :checkered_flag: Instalação
+
+Clone o projeto em seu ambiente Linux
+
+```bash
+git clone https://github.com/jamesandrade/certbot-nginx
+```
+
+Accesse o diretório
+
+```bash
+$ cd certbot-nginx
+```
+
+Edite o arquivo .env, passando as informações necessárias, com um editor de sua preferência
+Nesse arquivo:
+APPLICATION_DOMAIN é o domínio ao qual será instalada a aplicação;
+APPLICATION_MAIL é o email que receberá notificações;
+APPLICATION_REDIRECT é o comando para o qual o futuramente o nginx fará um proxy reverso, exemplo: http://localhost:3000
+
+```bash
+vim .env
+```
+
+Seja root
+
+```bash
+sudo su -
+```
+
+Execute o instalador
+
+```bash
+./install.sh
+```
+
+Após a conclusão do procedimento, você poderá editar o arquivo app.conf do nginx para melhor atender sua necessidade
+
+```bash
+vim nginx/config/app.conf
+```
+
+Reinicie o nginx para as alterações serem aplicadas
+
+```bash
+docker compose restart nginx
+```
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ -f .env ]; then
     set -a
     . ./.env
     set +a
-    mkdir -p certbot/conf/live/${APPLICATION_DOMAIN}
+    #mkdir -p certbot/conf/live/${APPLICATION_DOMAIN}
     echo "Criando config. nginx"
     printf "server {
     listen 80;
@@ -24,32 +24,31 @@ if [ -f .env ]; then
     }
 }" > "nginx/conf/app.conf"
     echo "Iniciando containers"
-    sudo docker compose up -d
+    docker compose up -d
     echo "Aguardando 10 segundos"
     sleep 10
 
     echo "Iniciando Primeira config. certbot"
-    sudo docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d ${APPLICATION_DOMAIN} --email ${APPLICATION_MAIL} --agree-tos --non-interactive
+    docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d ${APPLICATION_DOMAIN} --email ${APPLICATION_MAIL} --agree-tos --non-interactive
     echo "Iniciando Segunda config. certbot"
-    sudo docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${APPLICATION_DOMAIN} --email ${APPLICATION_MAIL} --agree-tos --non-interactive
+    docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${APPLICATION_DOMAIN} --email ${APPLICATION_MAIL} --agree-tos --non-interactive
     echo "Down nginx"
-    sudo docker compose down nginx
+    docker compose down nginx
     echo "Reescrevendo config. nginx para https"
     printf "server {
-    listen 443;
+    listen 443 ssl;
     server_name ${APPLICATION_DOMAIN};
-    ssl_certificate /etc/nginx/certificates/fullchain.pem;
-    ssl_certificate_key /etc/nginx/certificates/privkey.pem;
+    ssl_certificate /etc/nginx/certificates/fullchain1.pem;
+    ssl_certificate_key /etc/nginx/certificates/privkey1.pem;
     location / {
-    	proxy_pass ${APPLICATION_REDIRECT};
+        proxy_pass ${APPLICATION_REDIRECT};
     }
 }" > "nginx/conf/app.conf"
     echo "Aguardando 10 segundos"
     sleep 10
     echo "Iniciando nginx com as novas configs"
-    sudo docker compose up nginx -d
+    docker compose up nginx -d
     echo "adicionando cron para atualizar o certificado"
-    cat <(crontab -l) <(echo "0 0 1 * * sudo docker-compose run --rm certbot renew") | crontab -
-
+    cat <(crontab -l) <(echo "0 0 1 * * docker run --rm certbot renew && docker restart nginx") | crontab -
     echo "Finalizado!"
 fi
